20caf82498ab96fe1735584956945cc0aa2bf2c2
Andrea Ferro <77702904+Andrea-Estefania@users.noreply.github.com>
2022-03-22 13:15:39 -0400

    Add project files.

---
+++ MyFile.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+using System.IO;
+
+namespace A04
+{
+    class MyFile
+    {
+        private float fileSize;
+        private bool okayToWrite = true;
+        private string pathname;
+
+        public float FileSize
+        {
+            get 
+            {
+                return fileSize;
+            }
+            set
+            { 
+                fileSize = value; 
+            }
+        }
+
+        public string PathName
+        {
+            get
+            {
+                return pathname;
+            }
+            set
+            {
+                pathname = value;
+            }
+        }
+
+        public bool OkayToWrite
+        {
+            get
+            {
+                return okayToWrite;
+            }
+            set
+            {
+                okayToWrite = value;
+            }
+        }
+
+        /* 
+        Name	: MyFile -- Constructor
+        Purpose : to instantiate a new file object to keep track and store the information need for the program to write in the text file         
+        Inputs	:	  string newFilePath            Pathname of the text file
+                      float newSize                 Expected file of the file
+        Outputs	:	NONE
+        Returns	:	Nothing
+        */
+
+        public MyFile(string newFilePath, float newSize)
+        {
+            this.okayToWrite = true;
+            this.fileSize = newSize;
+            this.pathname = newFilePath;
+        }
+
+        /*
+         TITLE        : Random String Generator Returning Same String
+         AUTHOR       : RCIX
+         DATE         : 2009 - 07 - 13
+         VERSION      : N/A
+         AVAILABILITY : https://stackoverflow.com/questions/1122483/random-string-generator-returning-same-string
+         */
+
+        /*  
+       Name	    :   RandomData
+       Purpose  :   It is used to generate the random data that will be appended to the text file
+       Inputs	:	NONE
+       Outputs	:	NONE
+       Returns	:	string with the random data
+        */
+
+        public string RandomData()
+        {
+            StringBuilder theData = new StringBuilder();
+            char ch;
+            Random random = new Random((int)DateTime.Now.Ticks); // To help generate the random character
+
+            for (int i = 0; i < Constants.kArraySize; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))); // Generates the random character
+                theData.Append(ch); // Appends the random character to the final string
+            }
+
+            return theData.ToString();
+        }
+
+        /*  
+       Name	    :   WriteInFile
+       Purpose  :   It is used to write in the file. It makes use of a lock so just one thread has access to the file at the same time.
+       Inputs	:	NONE
+       Outputs	:	NONE
+       Returns	:	NONE
+        */
+
+        public void WriteInFile()
+        { 
+            string thePathName = this.@PathName;
+
+                while (this.okayToWrite == true) // Is it okay to keep writing in the text file?
+                {
+                    lock (this)
+                    {// Lock on the current instance of the class for current thread
+                        StreamWriter appendData = new StreamWriter(thePathName, true);
+                        string randomData = RandomData();
+                    try
+                        {
+                            appendData.WriteLine(randomData); // Appends the random data to the file
+
+                        }
+                        catch (IOException ex) // An I/O was caught 
+                        {
+                            Console.WriteLine("An I/O error occurred while opening the file. Exception caught {0}\n", ex);
+                        }
+                        catch (Exception ex) // Regular exceptions
+                        {
+                            Console.WriteLine("Exception caught: {0}\n", ex);
+                        }
+                        finally
+                        {
+                            appendData.Close(); // Closes the file
+                        }
+
+                        long fileSize = new FileInfo(thePathName).Length; // Gets the current size of the file
+                        float userSize = this.FileSize; // Size the user specified when the
+
+                        if (fileSize >= userSize) // Have we reach the size the user wants?
+                        {
+                            this.OkayToWrite = false;
+                        }
+                    } // Lock released for the next thread here
+                }
+        }
+
+        /*  
+        Name	    :   CurrentSize
+        Purpose     :   To print in the screen the current size of the file that's being written
+        Inputs	    :	NONE
+        Outputs	    :	Current size of the text file
+        Returns	    :	NONE
+        */
+
+        public void CurrentSize()
+        {
+            string myPath = this.@PathName;
+        
+            while (this.okayToWrite == true) //Is okay to keep showing the message?
+            {
+                long fileSize = new FileInfo(myPath).Length; // Gets the current size of the file
+
+                Console.WriteLine("The current file size is: {0} bytes.\n", fileSize.ToString());
+                Thread.Sleep(1000); // Sleeps for one second
+
+            }
+        }
+
+        /*  
+        Name	    :   GetFinalSize
+        Purpose     :   It is used to get the final size of the text file
+        Inputs	    :	NONE
+        Outputs	    :	NONE
+        Returns	    :	The final size of the text file
+        */
+
+        public long GetFinalSize()
+        {
+            string myPath = this.@PathName;
+            long finalSize = new FileInfo(myPath).Length;
+            return finalSize;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
+using System.IO;
+
+namespace A04
+{
+    static class Constants
+    {
+        public const float kEpsilon = 0.005f;
+        public const int kArraySize = 50;
+        public const float kMinSize = 1000;
+        public const float kMaxSize = 20000000;
+    }
+
+    /*
+     NAME      : MyThreads
+     PURPOSE   : The class has been created to create methods that allow the initialization of the threads that
+                 will write in the file and keep track of the size in the file to avoid duplicate code since the 
+                 same block of code would have been presented in two parts of the main. As well, it creates the 
+                 MyFile object to get the store the user input.
+     */
+
+    class MyThreads
+    {
+        /* 
+        Name	: StartMyThreads
+        Purpose : To initialize the threads that will write and keep track of the text file 
+        Inputs	:	string path            Pathname of the text file
+                    float newSize          Expected file of the file
+        Outputs	:	NONE
+        Returns	:	Nothing
+        */
+
+        public void StartMyThreads(string path, float size)
+        {
+            MyFile userFile = new MyFile(@path, size);
+
+            ThreadStart threadDelegate = new ThreadStart(userFile.WriteInFile); // Delegate to the write in file method
+            ThreadStart sizeDelegate = new ThreadStart(userFile.CurrentSize); // Delegate to current size method
+            Thread[] threadsArray = new Thread[Constants.kArraySize]; // Array of 50 identical threads
+
+            // Assignment of delegates to threads
+
+            Thread checkSize = new Thread(sizeDelegate);
+
+            for (int i = 0; i < Constants.kArraySize; i++)
+            {
+                Thread writingThread = new Thread(threadDelegate);
+                threadsArray[i] = writingThread;
+            }
+
+            // Start of the threads
+
+            checkSize.Start();
+
+            for (int i = 0; i < Constants.kArraySize; i++)
+            {
+                threadsArray[i].Start();
+            }
+
+            for (int i = 0; i < Constants.kArraySize; i++)
+            {
+                threadsArray[i].Join();
+            }
+
+            checkSize.Join();
+
+            long finalSize = userFile.GetFinalSize();
+
+            // Final size of the file
+
+            Console.WriteLine("The final size of the file is: {0} bytes.", finalSize.ToString());
+        }
+
+        /* 
+        Name	: Usage
+        Purpose : To show the user the usage of the program
+        Inputs	:   NONE
+        Outputs	:	Usage of the application
+        Returns	:	Nothing
+        */
+
+        public void Usage()
+        {
+            Console.WriteLine("USAGE:");
+            Console.WriteLine("A04.exe Pathname FileSize(between 1,000 and the other 20,000,000)");
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyThreads start = new MyThreads();
+
+            if (args.Length == 0 || args.Length > 2 || args.Length == 1) // Wrong amount of command line arguments
+            {
+                start.Usage();
+            }
+            else if (args[0] == "/?") // User asks for the usage
+            {
+                start.Usage();
+            }
+            else
+            {
+                float size = float.Parse(args[1]); 
+
+                if (size < Constants.kMinSize || size > Constants.kMaxSize) // Size is out range
+                {
+                    start.Usage();
+                }
+                else
+                {
+                    string checkPathName = @args[0];
+                    
+                    if (Directory.Exists(Path.GetDirectoryName(@args[0]))) // Directory exists?
+                    {
+                        MyFile userFile = new MyFile(@args[0], size);
+
+                        if (!File.Exists(@args[0])) // Text file does not exist
+                        {
+                            StreamWriter newFile = File.CreateText(@args[0]); // New text file
+                            newFile.Close();
+
+                            start.StartMyThreads(args[0], size);
+
+                        } else // Text File exists
+                        {
+                            long fileSize = new FileInfo(@args[0]).Length;
+                            bool sameSize = (Math.Abs(fileSize - size) < Constants.kEpsilon);
+
+                            if (sameSize) // Checks if the new file can receive random data
+                            {
+                                Console.WriteLine("The file exists and it is the same size. More random data cannot be added.\n");
+
+                            } else
+                            {
+                                start.StartMyThreads(args[0], size); // Start of the threads
+                            }
+                        }
+                    }
+                    else // Directory does not exist
+                    {
+                        try // Creates directory and file
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(@args[0]));
+                            StreamWriter newFile = File.CreateText(@args[0]);
+                            newFile.Close();
+                            start.StartMyThreads(args[0], size); // Starts the threads
+                        }
+                        catch (ArgumentException)
+                        {
+                            Console.WriteLine("Path cannot be empty string or all whitespace.\n");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("A exception was caught: {0}.\n", ex);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
